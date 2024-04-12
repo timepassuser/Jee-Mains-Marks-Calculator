d7ea86da70d527e8ae606282eb6f1d5f97240900
timepassuser <159204696+timepassuser@users.noreply.github.com>
2024-04-13 02:13:27 +0530

    removed corsproxy

--- proxies.js
+++ proxies.js
@@ -1,5 +1,4 @@
 // This file contains a list of proxies that can be used to fetch your response sheet
 proxies = [
-    "https://corsproxy2.vercel.app",
     "https://corsproxy-worker.timepassuser.workers.dev"
 ]
