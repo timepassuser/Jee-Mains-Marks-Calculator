7c4b89cc0d07cce9d13caef4a5f039b9efbc2775
timepassuser <159204696+timepassuser@users.noreply.github.com>
2025-02-04 20:09:39 +0530

    typo fix

--- proxies.js
+++ proxies.js
@@ -1,5 +1,5 @@
 // This file contains a list of proxies that can be used to fetch your response sheet
 proxies = [
     // "https://corsproxy-worker.timepassuser.workers.dev"
-    "http://corsproxy2.vercel.app"
+    "https://corsproxy2.vercel.app"
 ]
