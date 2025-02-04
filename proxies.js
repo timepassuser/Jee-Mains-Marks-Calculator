055f44788329d4d7d3795bea649cc354135c130f
timepassuser <159204696+timepassuser@users.noreply.github.com>
2025-02-04 20:33:59 +0530

    testing

--- proxies.js
+++ proxies.js
@@ -1,5 +1,6 @@
 // This file contains a list of proxies that can be used to fetch your response sheet
 proxies = [
     // "https://corsproxy-worker.timepassuser.workers.dev"
-    "https://jeemarkscalculator.vercel.app"
+    // "https://jeemarkscalculator.vercel.app"
+    "https://https://jeemarkscalculator.timepassuser.workers.dev"
 ]
